48c632d1253efd242b2fae1267b38330cebe5d6f
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-12 10:40:21 +0200

    refactor: move html attributes to other file

--- src/ngx-form-interface.htmlattributes.ts
+++ src/ngx-form-interface.htmlattributes.ts
@@ -1,4 +1,10 @@
 import { autocomplete, autofocus, disabled, input, readonly, required } from '@ngx-form/type';
+/**
+ * Some html attributes
+ * http://w3c.github.io/html-reference/elements.html
+ * @export
+ * @interface HtmlAttributesInterface
+ */
 export interface HtmlAttributesInterface {
   autocomplete?: autocomplete;
   autofocus?: autofocus;
