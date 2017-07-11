46f1f0d2253f7e27e04b1a5c40258bc19ef91734
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-11 19:51:27 +0200

    refactor: export validators and error-messages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 
 export * from './ngx-form-interface.config';
+export * from './ngx-form-interface.error-messages';
 export * from './ngx-form-interface.htmlattributes';
 export * from './ngx-form-interface.mdinput';
 export * from './ngx-form-interface';
+export * from './ngx-form-interface.validators';
