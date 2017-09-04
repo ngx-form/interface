351caa69699cb7ce35deb240e6ecade81e208ad9
Diabeu <scibor.rudnicki@ster-project.pl>
2017-09-04 16:24:07 +0200

    refactor(ConfigInterface): simplify interface structure

--- src/ngx-form-interface.config.ts
+++ src/ngx-form-interface.config.ts
@@ -2,31 +2,12 @@ import { element } from '@ngx-form/type';
 import { component } from '@ngx-core/common';
 import { ErrorMessagesInterface } from './ngx-form-interface.error-messages';
 
-/**
- * Property `FormElementConfigInterface` elements
- * @export
- * @interface ConfigElementInterface
- */
-export interface ConfigElementInterface {
-  name: element;
-  component: component;
-}
-
-/**
- * @export
- * @interface ConfigElementsInterface
- */
-export interface ConfigElementsInterface {
-  [index: number]: ConfigElementInterface;
-}
-
-/**
- *
- * Interface for FormElementModule forRoot parameter config
- * @export
- * @interface FormElementConfigInterface
- */
 export interface FormElementConfigInterface {
-  elements: ConfigElementsInterface;
+  elements: {
+    [index: number]: {
+      name: element;
+      component: component;
+    }
+  };
   errorMessages?: ErrorMessagesInterface;
 }
